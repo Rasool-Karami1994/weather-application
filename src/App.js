830ed111fb6399fed6069d53bcc6612ff79caecd
rasool700 <rasoolkarami700@gmail.com>
2022-06-30 19:42:19 +0430

    Api server changed because of Latency

--- src/App.js
+++ src/App.js
@@ -6,17 +6,14 @@ import { WiStrongWind } from "react-icons/wi";
 import { WiHumidity } from "react-icons/wi";
 import { MdOutlineVisibility } from "react-icons/md";
 import { WiCloudy } from "react-icons/wi";
-
+import API from "./WeatherAPI";
 function App() {
-  const apiKey = "5cc372fcccfe46d68db122001222906";
-  const [weatherData, setWeatherData] = useState([{}]);
+  const [weatherData, setWeatherData] = useState({});
   const [city, setCity] = useState("");
 
   const getWeather = (e) => {
     if (e.key === "Enter") {
-      fetch(
-        `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
-      )
+      fetch(`${API.base}weather?q=${city}&units=metric&APPID=${API.key}`)
         .then((response) => response.json())
         .then((data) => {
           setWeatherData(data);
@@ -25,14 +22,7 @@ function App() {
   };
 
   return (
-    <div
-      className={
-        typeof weatherData.location === "undefined" ||
-        weatherData.current.is_day === 1
-          ? "App"
-          : "App-nightmode"
-      }
-    >
+    <div className="App">
       <input
         type="text"
         placeholder="Enter Your City ..."
@@ -40,22 +30,22 @@ function App() {
         value={city}
         onKeyPress={getWeather}
       ></input>
-      {typeof weatherData.location === "undefined" ? (
+      {typeof weatherData.sys === "undefined" ? (
         <></>
       ) : (
         <div className="data-container">
           <p className="city">
             <MdLocationPin />
-            {weatherData.location.name}
+            {weatherData.name}
           </p>
-          <p className="condition"> {weatherData.current.condition.text}</p>
+          <p className="condition"> {weatherData.weather[0].main}</p>
           <p className="temp">
-            {weatherData.current.condition.text === "Sunny" ? (
+            {weatherData.weather[0].main === "Clear" ? (
               <IoMdSunny className="icon" />
             ) : (
               <WiCloudy />
             )}
-            {weatherData.current.temp_c}
+            {weatherData.main.temp}
             <span>&deg;C</span>
           </p>
 
@@ -63,17 +53,17 @@ function App() {
             <div>
               <WiStrongWind className="icon" />
               <p>Wind Speed</p>
-              <p>{weatherData.current.wind_kph} kph</p>
+              <p>{weatherData.wind.speed} kph</p>
             </div>
             <div>
               <WiHumidity className="icon" />
               <p>Humidity</p>
-              <p>{weatherData.current.humidity} %</p>
+              <p>{weatherData.main.humidity} %</p>
             </div>
             <div>
               <MdOutlineVisibility className="icon" />
               <p>Visibility</p>
-              <p>{weatherData.current.vis_km} Km</p>
+              <p>{weatherData.visibility} Km</p>
             </div>
           </div>
         </div>
---
+++ src/WeatherAPI.js
@@ -0,0 +1,6 @@
+const API = {
+  key: "9a8d44c320cb24300af9eef2f9a7dc0e",
+  base: "http://api.openweathermap.org/data/2.5/",
+};
+
+export default API;
